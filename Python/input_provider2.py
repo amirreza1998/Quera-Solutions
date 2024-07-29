fd55439226f21bcb783efa21ce36f61ed1b5ac23
amirreza <amirrezakazemloo1998@gmail.com>
2024-07-29 03:37:37 +0330

    add code for getting whole input data with of interval

---
+++ Python/input_provider2.py
@@ -0,0 +1,10 @@
+import sys
+
+input = sys.stdin.read
+data = input().split()
+data = list(map(int, data))
+n,q = data[:2]
+a = data[2:n + 2]
+query = data[n + 2:]
+####-----------in this type of inputting after entering all the input in mac/linux should use ctrl+D-----------####
+####-----------and in the windows should use ctrl+Z and enter key-----------####
