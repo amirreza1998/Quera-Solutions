7cbb072028ab8a8bf3be0a3333463135948aa1b8
amirreza <amirrezakazemloo1998@gmail.com>
2024-07-29 03:39:36 +0330

    add stdout for writting output

--- Python/input_provider2.py
+++ Python/input_provider2.py
@@ -8,3 +8,6 @@ a = data[2:n + 2]
 query = data[n + 2:]
 ####-----------in this type of inputting after entering all the input in mac/linux should use ctrl+D-----------####
 ####-----------and in the windows should use ctrl+Z and enter key-----------####
+
+###---code for write out output in command line---###
+sys.stdout.write('\n'.join(map(str, ["this", "is", "results"])) + '\n')
